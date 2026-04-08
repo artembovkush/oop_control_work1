29af63b11011cb483c66009aa1f530b7eef23c6e
Usern <bracto0@gmail.com>
2026-04-08 14:13:47 +0300

    1

--- src/Car.java
+++ src/Car.java
@@ -1,15 +1,15 @@
 public class Car {
 
-    String brand; //
-    int year; //
-    int speed; //
+    String brand; //бренд автомобіля
+    short year; // рік його вироботку
+    double speed; // максимальна швинкість
 
    public void startEngine() {
         System.out.println("Двигун запускаеться");
     }
 
     public void accelerate() {
-        speed += 10;
+        speed += 10.5;
         System.out.println("машина набирае швидкість" + speed);
     }
 }
